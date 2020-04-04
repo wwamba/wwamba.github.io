72e30743636b9a1efe55662e575fced6b9a495ce
wwamba <58701577+wwamba@users.noreply.github.com>
2020-04-04 21:07:55 +0100

    Add files via upload

---
+++ js/ww-javascript-en.js
@@ -0,0 +1,3 @@
+function play(){
+document.getElementById('vidwrap').innerHTML = '<iframe width="100%" height="100%"  src="https://www.youtube.com/embed/nOZJ5TYbF-Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
